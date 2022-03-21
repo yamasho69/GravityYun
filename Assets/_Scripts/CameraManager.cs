e7740ee57bb0c23823d6d4e8d6fdb005f9d79137
yamasho69 <nitiham55@yahoo.co.jp>
2022-03-22 00:18:53 +0900

    3回目のコミット
    動画分は終了。5ステージ実装→敵、穴、BGM、アニメーション、ボイス、説明文を前のゲームから移植、ボタンと一時停止も。

--- Assets/_Scripts/CameraManager.cs
+++ Assets/_Scripts/CameraManager.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] Camera cam;
+    [SerializeField] bool goRight;
+    [SerializeField] bool goLeft;
+    float cameraHorizontalMove = 18.0f;
     //プレイヤーが左から右に移動：カメラを右に移動
     //プレイヤーが右から左に移動：カメラを左に移動
-    private void OnTriggerExit2D(Collider2D collision) {
+    private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.tag == "Player") {
-            if(collision.transform.position.x > transform.position.x) {
-                //カメラを右に移動
-                cam.transform.position = new Vector3(cam.transform.position.x+18, 0, -10);
+            if(collision.transform.position.x < transform.position.x) {
+                //左側から触った　プレイヤーx<オブジェクトx　＝　右に行こうとしている
+                goRight = true;
             } else {
+                //右側から触った　プレイヤーx>オブジェクトx　＝　左に行こうとしている
+                goLeft = true;
+            }
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            // 左側から触って(goRight)、右側に出た(プレイヤーx>オブジェクトx)
+            if (collision.transform.position.x > transform.position.x&&goRight) {
+                //カメラを右に移動
+                cam.transform.position = new Vector3(cam.transform.position.x + cameraHorizontalMove, 0, -10);
+                goRight = false;
+            // 右側から触って(goLeft)、左側に出た(プレイヤーx<オブジェクトx)
+            } else if(collision.transform.position.x < transform.position.x &&goLeft) {
                 //カメラを左に移動
-                cam.transform.position = new Vector3(cam.transform.position.x-18, 0, -10);
+                cam.transform.position = new Vector3(cam.transform.position.x - cameraHorizontalMove, 0, -10);
+                goLeft = false;
+            //触った方向から出た
+            } else {
+                goLeft = false;
+                goRight = false;
             }
         }
 
